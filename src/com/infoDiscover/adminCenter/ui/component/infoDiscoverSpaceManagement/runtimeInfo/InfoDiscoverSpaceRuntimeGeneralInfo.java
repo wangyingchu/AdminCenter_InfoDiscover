be2ce83c5c26c34a557f3bc09c6a5e46a6be4a42
wangychu <yingchu-wang@163.com>
2017-07-05 17:33:27 +0800

    Apply business solution UI logic

--- src/com/infoDiscover/adminCenter/ui/component/infoDiscoverSpaceManagement/runtimeInfo/InfoDiscoverSpaceRuntimeGeneralInfo.java
+++ src/com/infoDiscover/adminCenter/ui/component/infoDiscoverSpaceManagement/runtimeInfo/InfoDiscoverSpaceRuntimeGeneralInfo.java
@@ -1,13 +1,19 @@
 package com.infoDiscover.adminCenter.ui.component.infoDiscoverSpaceManagement.runtimeInfo;
 
+import com.infoDiscover.adminCenter.logic.component.businessSolutionManagement.BusinessSolutionOperationUtil;
+import com.infoDiscover.adminCenter.ui.component.common.ConfirmDialog;
 import com.infoDiscover.adminCenter.ui.component.common.MainSectionTitle;
 import com.infoDiscover.adminCenter.ui.component.common.SecondarySectionTitle;
 import com.infoDiscover.adminCenter.ui.component.infoDiscoverSpaceManagement.InfoDiscoverSpaceDetail;
 import com.infoDiscover.adminCenter.ui.util.UserClientInfo;
 import com.infoDiscover.infoDiscoverEngine.util.helper.DiscoverSpaceStatisticMetrics;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.FontAwesome;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+
+import java.util.List;
 
 /**
  * Created by wangychu on 10/3/16.
@@ -26,6 +32,9 @@ public class InfoDiscoverSpaceRuntimeGeneralInfo extends VerticalLayout {
     private InfoDiscoverSpaceDetail parentInfoDiscoverSpaceDetail;
     private String discoverSpaceName;
     private MainSectionTitle mainSectionTitle;
+    private MenuBar.MenuItem businessSolutionSelectRootItem;
+    private MenuBar businessSolutionSelectorMenuBar;
+    private MenuBar.Command applyBusinessSolutionMenuItemCommand;
 
     public InfoDiscoverSpaceRuntimeGeneralInfo(UserClientInfo currentUserClientInfo){
         this.currentUserClientInfo=currentUserClientInfo;
@@ -110,9 +119,24 @@ public class InfoDiscoverSpaceRuntimeGeneralInfo extends VerticalLayout {
         spaceDivLayout.setWidth("15px");
         actionButtonsPlacementLayout.addComponent(spaceDivLayout);
 
+        this.businessSolutionSelectorMenuBar = new MenuBar();
+        this.businessSolutionSelectorMenuBar.addStyleName(ValoTheme.MENUBAR_SMALL);
+        this.businessSolutionSelectorMenuBar.addStyleName(ValoTheme.MENUBAR_BORDERLESS);
+        actionButtonsPlacementLayout.addComponent(this.businessSolutionSelectorMenuBar);
+        this.applyBusinessSolutionMenuItemCommand = new MenuBar.Command() {
+            public void menuSelected(MenuBar.MenuItem selectedItem) {
+                String selectedCommandName=selectedItem.getText();
+                applyBusinessSolution(selectedCommandName);
+            }
+        };
+        businessSolutionSelectRootItem=this.businessSolutionSelectorMenuBar.addItem("启用业务解决方案", VaadinIcons.PACKAGE, null);
+
+        Label spaceDivLabel2=new Label("|");
+        actionButtonsPlacementLayout. addComponent(spaceDivLabel2);
+
         Button deleteDiscoverSpaceButton=new Button("删除此信息发现空间");
         deleteDiscoverSpaceButton.addStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
-        deleteDiscoverSpaceButton.addStyleName(ValoTheme.BUTTON_TINY);
+        deleteDiscoverSpaceButton.addStyleName(ValoTheme.BUTTON_SMALL);
         deleteDiscoverSpaceButton.setIcon(FontAwesome.TRASH_O);
         deleteDiscoverSpaceButton.addClickListener(new Button.ClickListener() {
             @Override
@@ -158,6 +182,22 @@ public class InfoDiscoverSpaceRuntimeGeneralInfo extends VerticalLayout {
         this.spaceDataDiskSize.setReadOnly(false);
         this.spaceDataDiskSize.setValue(""+discoverSpaceStatisticMetrics.getSpaceDiskSize());
         this.spaceDataDiskSize.setReadOnly(true);
+
+        int totalDataTypeCount=discoverSpaceStatisticMetrics.getDimensionsStatisticMetrics().size()+
+                discoverSpaceStatisticMetrics.getFactsStatisticMetrics().size()+
+                discoverSpaceStatisticMetrics.getRelationsStatisticMetrics().size();
+        if(totalDataTypeCount>0){
+            businessSolutionSelectRootItem.setEnabled(false);
+        }else{
+            businessSolutionSelectRootItem.removeChildren();
+            List<String> solutionsList= BusinessSolutionOperationUtil.getExistBusinessSolutions();
+            if(solutionsList!=null){
+                for(String currentSolution:solutionsList){
+                    businessSolutionSelectRootItem.addItem(currentSolution, VaadinIcons.CLIPBOARD_TEXT, this.applyBusinessSolutionMenuItemCommand);
+                }
+            }
+            businessSolutionSelectRootItem.setEnabled(true);
+        }
     }
 
     public void setParentInfoDiscoverSpaceDetail(InfoDiscoverSpaceDetail parentInfoDiscoverSpaceDetail) {
@@ -166,5 +206,22 @@ public class InfoDiscoverSpaceRuntimeGeneralInfo extends VerticalLayout {
 
     public void setDiscoverSpaceName(String discoverSpaceName) {
         this.discoverSpaceName = discoverSpaceName;
+    }
+
+    private void applyBusinessSolution(String businessSolutionName){
+        //do apply solution logic
+        String confirmMessageString=" 请确认是否在信息发现空间 <b>"+this.discoverSpaceName+"</b> 中启用业务解决方案 <b>"+businessSolutionName+"</b> ,执行启用操作将会在信息发现空间中创建该业务解决方案模板中定义的所有维度类型,事实类型,关系类型以及其他数据关联映射信息和自定义属性别名信息。<br/>注意：此项操作不可撤销执行结果。";
+        Label confirmMessage=new Label(FontAwesome.INFO.getHtml()+confirmMessageString, ContentMode.HTML);
+        final ConfirmDialog applyBusinessSolutionConfirmDialog = new ConfirmDialog();
+        applyBusinessSolutionConfirmDialog.setConfirmMessage(confirmMessage);
+        Button.ClickListener confirmButtonClickListener = new Button.ClickListener() {
+            @Override
+            public void buttonClick(final Button.ClickEvent event) {
+                //close confirm dialog
+                applyBusinessSolutionConfirmDialog.close();
+            }
+        };
+        applyBusinessSolutionConfirmDialog.setConfirmButtonClickListener(confirmButtonClickListener);
+        UI.getCurrent().addWindow(applyBusinessSolutionConfirmDialog);
     }
 }
