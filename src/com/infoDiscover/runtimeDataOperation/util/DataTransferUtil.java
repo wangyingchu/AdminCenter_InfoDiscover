5b9c2c5efab7ea529698f2a49d29d4fc00dc22f8
wangychu <yingchu-wang@163.com>
2017-08-03 20:17:29 +0800

    code update

--- src/com/infoDiscover/runtimeDataOperation/util/DataTransferUtil.java
+++ src/com/infoDiscover/runtimeDataOperation/util/DataTransferUtil.java
@@ -84,6 +84,7 @@ public class DataTransferUtil {
     public static boolean importTypeDate(String discoverSpaceName,String dataTypeKind,String dataTypeName,List<DataTypePropertyInfoVO> dataPropertiesList){
         DataImporter importer = new DataImporter(discoverSpaceName);
         String jsonContent=transferTypeDataToJson(dataTypeKind,dataTypeName,dataPropertiesList);
+        System.out.println(jsonContent);
         try {
             importer.importData(jsonContent,true);
             return true;
@@ -96,24 +97,34 @@ public class DataTransferUtil {
 
     public static void main(String[] args){
         List<DataTypePropertyInfoVO> propertyList=new ArrayList<>();
-        propertyList.add(new DataTypePropertyInfoVO("fact001p2",new Boolean(false)));
-
-        //propertyList.add(new DataTypePropertyInfoVO("fact001p2",new Date()));
+        //propertyList.add(new DataTypePropertyInfoVO("fact001p2",new Boolean(false)));
+/*
+        propertyList.add(new DataTypePropertyInfoVO("fact001p2",new Date()));
         propertyList.add(new DataTypePropertyInfoVO("fact003p1",new Float(223399)));
         propertyList.add(new DataTypePropertyInfoVO("sourceStr","AAA"));
         propertyList.add(new DataTypePropertyInfoVO("strprop",new Long(2233)));
-
         propertyList.add(new DataTypePropertyInfoVO("date01",new Date()));
-
-       // importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_FACT,"fact001",propertyList);
-
-        importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_FACT,"dupsource",propertyList);
-
-
+        propertyList.add(new DataTypePropertyInfoVO("booleanData",true));
+*/
+        //importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_FACT,"fact001",propertyList);
+        //importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_FACT,"dupsource",propertyList);
+        //importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_DIMENSION,"dupsourceDim",propertyList);
         //importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_DIMENSION,"dim001",propertyList);
-
-
         //importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_FACT,"fact001shouldwroing11",propertyList);
+
+        //propertyList.add(new DataTypePropertyInfoVO("strMatchKey","MATCHKEY-VALUE"));
+        //propertyList.add(new DataTypePropertyInfoVO("intMatchKey",new Integer(150)));
+        //propertyList.add(new DataTypePropertyInfoVO("longMatchKey",new Long(150)));
+        //propertyList.add(new DataTypePropertyInfoVO("booleanMatchKey",new Boolean(true)));
+        //propertyList.add(new DataTypePropertyInfoVO("doubleMatchKey",new Double(150)));
+        //propertyList.add(new DataTypePropertyInfoVO("floatMatchKey",new Float(150)));
+        //propertyList.add(new DataTypePropertyInfoVO("shortMatchKey",(short)10));
+        propertyList.add(new DataTypePropertyInfoVO("dateProp",new Date()));
+
+        importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_DIMENSION,"relSourceDimension",propertyList);
+
+        //importTypeDate("testApply",InfoDiscoverSpaceOperationUtil.TYPEKIND_FACT,"relSourceFact",propertyList);
+
 
     }
 }
